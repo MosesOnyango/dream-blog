2f99c24339e8045975990844dbb199dc555316df
Daniel Otieno <danielotieno.ke@gmail.com>
2019-04-08 18:12:25 +0300

    Validate form

--- accounts/views.py
+++ accounts/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from django.contrib.auth.forms import UserCreationForm
+from django.contrib import messages
 
 
 def register(request):
-    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            username = form.cleaned_data.get('username')
+            messages.success(request, f'Account created for {username}!')
+
+    else:
+        form = UserCreationForm()
 
     context = {
         'form': form
