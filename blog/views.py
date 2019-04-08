6b262c2857513f5ce9050a4016d4d6df571f85b3
Daniel Otieno <danielotieno.ke@gmail.com>
2019-04-09 02:07:27 +0300

    Fetch posts from database

--- blog/views.py
+++ blog/views.py
@@ -1,22 +1,10 @@
 from django.shortcuts import render
 
-posts = [
-    {
-        'author': 'Daniel Otieno',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+from .models import Post
 
 
 def home(request):
+    posts = Post.objects.all()
     context = {
         'posts': posts
     }
