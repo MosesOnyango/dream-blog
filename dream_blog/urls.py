61ca1f6aca1ab60233fce806666361b5e2f83795
Daniel Otieno <danielotieno.ke@gmail.com>
2019-04-08 21:35:00 +0300

    Create Login page

--- dream_blog/urls.py
+++ dream_blog/urls.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
+from django.contrib.auth import views as auth_view
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('login/', auth_view.LoginView.as_view(template_name='accounts/login.html'), name="login"),
+    path('logout/', auth_view.LogoutView.as_view(template_name='accounts/logout.html'), name="logout"),
     path('', include('blog.urls')),
     path('', include('accounts.urls')),
 ]
