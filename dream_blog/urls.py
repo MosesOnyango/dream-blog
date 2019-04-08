7093d1ce1f6cf68b04e4d940bfb6003c7e6017ce
Daniel Otieno <danielotieno.ke@gmail.com>
2019-04-08 23:22:06 +0300

    Add static media url

--- dream_blog/urls.py
+++ dream_blog/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth import views as auth_view
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -8,4 +10,4 @@ urlpatterns = [
     path('logout/', auth_view.LogoutView.as_view(template_name='accounts/logout.html'), name="logout"),
     path('', include('blog.urls')),
     path('', include('accounts.urls')),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
