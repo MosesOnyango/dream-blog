d362c186fa7eee3c736bdcc5f2ec9f32b5faab5f
Daniel Otieno <danielotieno.ke@gmail.com>
2019-04-09 00:03:21 +0300

    Create signals for new user

--- dream_blog/urls.py
+++ dream_blog/urls.py
@@ -10,4 +10,8 @@ urlpatterns = [
     path('logout/', auth_view.LogoutView.as_view(template_name='accounts/logout.html'), name="logout"),
     path('', include('blog.urls')),
     path('', include('accounts.urls')),
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
