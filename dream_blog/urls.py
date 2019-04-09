ab9799587b96305c20ede9783b3e9d2e09fded98
Daniel Otieno <danielotieno.ke@gmail.com>
2019-04-10 01:07:09 +0300

    Add functionality for user to be able to reset password

--- dream_blog/urls.py
+++ dream_blog/urls.py
@@ -6,8 +6,24 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+
     path('login/', auth_view.LoginView.as_view(template_name='accounts/login.html'), name="login"),
-    path('logout/', auth_view.LogoutView.as_view(template_name='accounts/logout.html'), name="logout"),
+
+    path('logout/', auth_view.LogoutView.as_view(
+        template_name='accounts/logout.html'), name="logout"),
+
+    path('password-reset/', auth_view.PasswordResetView.as_view(
+        template_name='accounts/password_reset.html'), name="password-reset"),
+
+    path('password-reset/done', auth_view.PasswordResetDoneView.as_view(
+        template_name='accounts/password_reset_done.html'), name="password-reset-done"),
+
+    path('password-reset-confirm/<uidb64>/<token>/', auth_view.PasswordResetConfirmView.as_view(
+        template_name='accounts/password_reset_confirm.html'), name="password-reset-confirm"),
+
+    path('password-reset-complete/', auth_view.PasswordResetCompleteView.as_view(
+        template_name='accounts/password_reset_complete.html'), name="password-reset-complete"),
+
     path('', include('blog.urls')),
     path('', include('accounts.urls')),
 ]
